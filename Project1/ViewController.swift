98ba4d3033f1dee436570f57af023044935ceb07
VNavrockij <vitalik.nav@gmail.com>
2022-08-15 16:51:31 +0300

    add: FileManager and Bundle.main.resourcePath

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        /** The line let fm = FileManager.default declares a constant called fm and assigns it the value returned by FileManager.default. This is a data type that lets us work with the filesystem, and in our case we'll be using it to look for files. */
+        let fm = FileManager.default
+        /** The line let path = Bundle.main.resourcePath! declares a constant called path that is set to the resource path of our app's bundle. Remember, a bundle is a directory containing our compiled program and all our assets. So, this line says, "tell me where I can find all those images I added to my app." */
+        let path = Bundle.main.resourcePath!
+        /** The line let items = try! fm.contentsOfDirectory(atPath: path) declares a third constant called items that is set to the contents of the directory at a path. Which path? Well, the one that was returned by the line before. As you can see, Apple's long method names really does make their code quite self-descriptive! The items constant will be an array of strings containing filenames. */
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                print("Ok")
+            }
+        }
     }
 
 
