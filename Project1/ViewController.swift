66075a39ed8142b55c5c632b0710bdde60273bcb
VNavrockij <vitalik.nav@gmail.com>
2022-08-16 12:27:57 +0300

    add: reuseble cell with info about pictures

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     var pictures = [String]()
 
@@ -22,10 +22,22 @@ class ViewController: UIViewController {
         
         for item in items {
             if item.hasPrefix("nssl") {
-                print("Ok")
+                // this is a picture to load!
+                pictures.append(item)
                 
             }
         }
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Ricture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
     }
 
 
